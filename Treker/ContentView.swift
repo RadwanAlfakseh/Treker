4d48be17cba3fca2c8e01d01c5b04be448844f6a
Radwan Alfakseh <Radwawn.alfakseh@gmail.com>
2021-10-21 20:15:17 +0200

    Initial Commit

---
+++ Treker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Treker
+//
+//  Created by Radwan Alfakseh on 21/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Treker/TrekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekerApp.swift
+//  Treker
+//
+//  Created by Radwan Alfakseh on 21/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct TrekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
